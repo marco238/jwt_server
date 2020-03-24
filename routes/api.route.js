59b82b4e772a170896e1562c370a0db2c331a291
Marcos <marcos.monzon@alten.es>
2020-03-24 11:37:53 +0100

    feat: server connection to mLab DB

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const db = "mongodb://jwtuser:jwtuser1@ds135068.mlab.com:35068/jwt_db";
+
+const mongoClientOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+mongoose.connect(db, mongoClientOptions, err => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Connected to mongodb');
+  }
+})
+
  router.get('/', (req, res) => {
-    res.send('From API route');
+    res.send('Hello from API route');
  })
 
  module.exports = router;
