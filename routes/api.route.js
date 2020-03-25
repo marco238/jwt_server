1f42fd9a5bdb9cb9ac810c4a56fbf25965c94dcf
Marcos <marcos.monzon@alten.es>
2020-03-25 12:12:13 +0100

    feat: middleware to verify token

--- routes/api.route.js
+++ routes/api.route.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const { registerUser, loginUser } = require('../controllers/user.controller');
 const { fetchEvents, fetchSpecials } = require('../controllers/event.controller');
+const { verifyToken } = require('../middlewares/specials.middleware');
 
  router.post('/register', registerUser);
  router.post('/login', loginUser);
  router.get('/events', fetchEvents);
- router.get('/specials', fetchSpecials);
+ router.get('/specials', verifyToken, fetchSpecials);
 
  module.exports = router;
